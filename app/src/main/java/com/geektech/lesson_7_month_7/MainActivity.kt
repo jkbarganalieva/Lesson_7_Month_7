f860a1b8a305c5df956a5d9885f1c503d036c390
jkbarganalieva <jkbarganalieva@gmail.com>
2023-05-26 05:46:16 +0600

    ncnhg

--- app/src/main/java/com/geektech/lesson_7_month_7/MainActivity.kt
+++ app/src/main/java/com/geektech/lesson_7_month_7/MainActivity.kt
@@ -5,11 +5,15 @@ import android.os.Bundle
 
 class MainActivity : AppCompatActivity() {
     // task1
+    //hkjb,b,
+    //jkg,hvmv
     //hktdjgcmhgmjh
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
     }
     // task 1
+    //hfmhfdcngfc
+    //bmv,b,jb
     //jhv,vb,kh.
 }
